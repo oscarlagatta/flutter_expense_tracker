e607550906cfa030c206860cb1a7564b2074fd79
Oscar Nicolas Lagatta <oscarlagatta@gmail.com>
2024-02-06 12:23:20 +0000

    adding dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,32 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
 );
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
+
 void main() {
   runApp(
     MaterialApp(
+        darkTheme: ThemeData.dark().copyWith(
+          // dark() no longer takes any arguments
+          colorScheme: kDarkColorScheme,
+          cardTheme: const CardTheme().copyWith(
+            color: kDarkColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 8,
+            ),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kDarkColorScheme.primaryContainer,
+              foregroundColor: kDarkColorScheme.onPrimaryContainer,
+            ),
+          ),
+        ),
         theme: ThemeData().copyWith(
-            useMaterial3: true,
             colorScheme: kColorScheme,
             appBarTheme: const AppBarTheme().copyWith(
               backgroundColor: kColorScheme.onPrimaryContainer,
@@ -29,11 +50,12 @@ void main() {
             ),
             textTheme: ThemeData().textTheme.copyWith(
                   titleLarge: TextStyle(
-                    fontWeight: FontWeight.normal,
+                    fontWeight: FontWeight.bold,
                     color: kColorScheme.onSecondaryContainer,
-                    fontSize: 14,
+                    fontSize: 16,
                   ),
                 )),
+        // themeMode: ThemeMode.system,
         home: const Expenses()),
   );
 }
